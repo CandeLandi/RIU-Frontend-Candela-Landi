f3322c0e857982d243b0c58cf50d683afd97eeb6
LandiCandela <clandi@vippinn.com>
2026-08-08 11:31:14 -0300

    chore: add base project structure

---
+++ src/app/heroes/components/hero-actions/hero-actions.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-actions',
+  imports: [],
+  templateUrl: './hero-actions.html',
+  styleUrl: './hero-actions.scss',
+})
+export class HeroActions {}
