e37789e4396cae4f950a77ccaed1768e4408ce2f
LandiCandela <clandi@vippinn.com>
2026-08-09 21:49:11 -0300

    chore: remove unused component

--- src/app/heroes/components/hero-actions/hero-actions.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-hero-actions',
-  imports: [],
-  templateUrl: './hero-actions.html',
-  styleUrl: './hero-actions.scss',
-})
-export class HeroActions {}
